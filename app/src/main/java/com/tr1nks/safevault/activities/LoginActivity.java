b7f66e165532b3724b6ac029aa4beb92f7b7e7f2
Tr1nks <tr1nks96@gmail.com>
2017-09-06 02:09:51 +0300

    init

---
+++ app/src/main/java/com/tr1nks/safevault/activities/LoginActivity.java
@@ -0,0 +1,64 @@
+package com.tr1nks.safevault.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import com.tr1nks.safevault.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+//        ((CheckBox) findViewById(R.id.showPasswCheckBox)).setOnCheckedChangeListener(this::showPasswOnCheckedChangeListener);
+        ((CheckBox) findViewById(R.id.showPasswCheckBox)).setOnCheckedChangeListener(showPasswOnCheckedChangeListener());
+    }
+
+    private CompoundButton.OnCheckedChangeListener showPasswOnCheckedChangeListener() {
+        return new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                EditText editText = ((EditText) findViewById(R.id.passwordEditText));
+                int pos = editText.getSelectionEnd();
+                if (b) {
+                    editText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                } else {
+                    editText.setInputType(129);
+                }
+                editText.setSelection(pos);
+            }
+        };
+    }
+
+//    /**
+//     * показать спрятать пароль обработчик checkbox
+//     *
+//     * @param compoundButton checkbox
+//     * @param b              checkbox is checked
+//     */
+//        private void showPasswOnCheckedChangeListener(CompoundButton compoundButton, boolean b) {
+//            EditText editText = ((EditText) findViewById(R.id.passwordEditText));
+//            int pos = editText.getSelectionEnd();
+//            if (b) {
+//                editText.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+//            } else {
+//                editText.setInputType(129);
+//            }
+//            editText.setSelection(pos);
+//        }
+
+    public void okButtonHandler(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+//        intent.putExtra("name", name.getText().toString()); // указываем первым параметром ключ, а второе значение
+//        intent.putExtra("lastname", lastName.getText().toString());  // по ключу мы будем получать значение с Intent
+        //todo password check
+        // todo if password - decode data
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/tr1nks/safevault/activities/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tr1nks.safevault.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.tr1nks.safevault.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
