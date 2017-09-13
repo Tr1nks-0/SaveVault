4337bb3a943d09fddcd9fda8ee99bb9560d6d7d5
Tr1nks <tr1nks96@gmail.com>
2017-09-14 01:48:16 +0300

    +fields

--- app/src/main/java/com/tr1nks/safevault/entities/DateTimeField.java
+++ app/src/main/java/com/tr1nks/safevault/entities/DateTimeField.java
@@ -1,6 +1,6 @@
 package com.tr1nks.safevault.entities;
 
-public class EditTextField extends Field {
+public class DateTimeField extends Field {
     public static final String LAYOUT_FILENAME = "field_edit_text";
     private static final String TITLE_ID_TAG = "titleForTextEditText";
     private static final String EDIT_TEXT_ID_TAG = "textEditText";
---
+++ app/src/main/java/com/tr1nks/safevault/entities/MultilineTextField.java
@@ -0,0 +1,7 @@
+package com.tr1nks.safevault.entities;
+
+public class MultilineTextField extends Field {
+    public static final String LAYOUT_FILENAME = "field_multiline_text";
+    private static final String TITLE_ID_TAG = "titleForMultilineEditText";
+    private static final String EDIT_TEXT_ID_TAG = "multilineEditText";
+}
