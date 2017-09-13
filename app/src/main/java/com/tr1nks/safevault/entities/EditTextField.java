83ef5b11f240bd7bb774849a85555adb7721966b
Tr1nks <tr1nks96@gmail.com>
2017-09-13 23:58:38 +0300

    +field - remaking layout

---
+++ app/src/main/java/com/tr1nks/safevault/entities/EditTextField.java
@@ -0,0 +1,5 @@
+package com.tr1nks.safevault.entities;
+
+public class EditTextField extends Field {
+    public static final String LAYOUT_FILENAME = "field_edit_text";
+}
--- app/src/main/java/com/tr1nks/safevault/entities/Field.java
+++ app/src/main/java/com/tr1nks/safevault/entities/Field.java
@@ -1,4 +1,19 @@
 package com.tr1nks.safevault.entities;
 
 public abstract class Field {
+    /*
+    * note (текст заметка)
+    * text (текст)
+    * number (номер)
+    * login (логин)
+    * password (пароль)
+    * one time password (одноразовый пароль)
+    * site (url) (сайт)
+    * mail (адрес почты)
+    * phone (телефон)
+    * date (дата)
+    * pin (пин)
+    * secret ---???? (??? секрет ???)
+    * app - later (приложение)
+    * */
 }
